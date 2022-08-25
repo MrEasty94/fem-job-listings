ba79259cd953460264522014622172efb966e39b
Michael East <michael@michaeleast.dev>
2022-08-25 14:21:25 +0100

    Initial upload of the React Site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import JobList from './components/JobList';
+import SearchBar from './components/SearchBar';
 
 function App() {
+  let jobTags = [];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header className="main-header"></header>
+      {jobTags ? <SearchBar /> : null}
+
+      <JobList />
+    </>
   );
 }
 
---
+++ src/components/JobListItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function JobListItem() {
+  return <div>JobListItem</div>;
+}
+
+export default JobListItem;
