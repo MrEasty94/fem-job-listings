62ffb6c049b2a51e35b3f34d13173fc8859a3b6d
Michael East <michael@michaeleast.dev>
2022-08-25 14:41:12 +0100

    Added images and new components for the list items.

---
+++ src/components/JobListInfo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function JobListInfo() {
+  return <div>JobListInfo</div>;
+}
+
+export default JobListInfo;
---
+++ src/components/JobListTags.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function JobListTags() {
+  return <div>JobListTags</div>;
+}
+
+export default JobListTags;
